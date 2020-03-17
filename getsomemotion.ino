7c2d5b8584283f65cda5a2770c0ac42b59885fad
c0dde <49539416+c0dde@users.noreply.github.com>
2020-03-18 10:10:38 +1100

    Update getsomemotion.ino

--- getsomemotion.ino
+++ getsomemotion.ino
@@ -2,20 +2,25 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  //read pin 0
   pinMode(0, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if pin voltage is high
   if (digitalRead(0) == HIGH) {
+    // submit the active record
     String IsActive = "1";
     Particle.publish("IsActive", IsActive, PRIVATE);
   }
+   // if pin voltage is low
   else {
+    // submit the inactive record
     String IsActive = "0";
     Particle.publish("IsActive", IsActive, PRIVATE);
   }
-  delay(1000);
+  //delay 2 seconds
+  delay(2000);
 }
 
 
